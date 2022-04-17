5f525877a71e78ed11bb5ed5990cc5db84720ccd
Matthew W <fatboychummy@gmail.com>
2022-04-16 19:28:05 -0600

    (hopefully) finish the save/loader. Will need some testing.

--- SimplifyDig.lua
+++ SimplifyDig.lua
@@ -51,17 +51,24 @@
     * The amount of steps completed as of the last savepoint
     * The last known position and facing of the turtle (x y z f)
     * The starting position (If GPS is enabled, it will be the global position).
+      (x y z f)
 
   Save files look like the following:
     args -flags --flags --flags=flags
     32
     3 8 -14 2
-    0 0 0
+    0 0 0 3
 ]]
 
 local steps = 0
 local skips = 0
 local pos = {
+  x = 0,
+  y = 0,
+  z = 0,
+  facing = 0
+}
+local start = {
   x = 0,
   y = 0,
   z = 0,
@@ -196,24 +203,108 @@ end
 -- Parse arguments.
 local args = parse(...)
 
+local function tokenize(...)
+    local line = table.concat({ ... }, " ")
+    local tokens = {}
+    local quoted = false
+    for match in string.gmatch(line .. "\"", "(.-)\"") do
+        if quoted then
+            table.insert(tokens, match)
+        else
+            for _match in string.gmatch(match, "[^ \t]+") do
+                table.insert(tokens, _match)
+            end
+        end
+        quoted = not quoted
+    end
+    return table.unpack(tokens)
+end
+
 --[[
   Save files look like the following:
     args -flags --flags --flags=flags
     32
     3 8 -14 2
-    0 0 0
+    0 0 0 3
 ]]
 local function load(filename)
+  -- read lines
+  local lines = {}
+  for line in io.lines(filename) do
+    table.insert(lines, line)
+  end
 
+  -- parse the arguments
+  local args = parse(tokenize(lines[1]))
+
+  -- get number of steps taken
+  local stepsTaken = tonumber(lines[2])
+
+  -- get position and facing
+  local posSaved = {}
+  posSaved.x, posSaved.y, posSaved.z, posSaved.facing = string.match(lines[3], "(%d+) (%d+) (%d+) (%d+)")
+
+  -- get the starting position and facing.
+  local startSaved = {}
+  startSaved.x, startSaved.y, startSaved.z, startSaved.facing = string.match(lines[4], "(%d+) (%d+) (%d+) (%d+)")
+
+  -- return all data
+  return {
+    args = args,
+    stepsTaken = stepsTaken
+    posSaved = posSaved,
+    startSaved = startSaved
+  }
 end
 
 local function save(filename, args)
   local h = io.open(filename, 'w')
+
+  -- write the arguments.
   h:write(table.concat(args.args, ' '))
   h:write(' ')
-  local flags = {}
-  for k, v in pairs(flags) do
 
+  -- determine the flags
+  local oneflags = {}
+  local longflags = {}
+  local equalflags = {}
+  for k, v in pairs(args.flags) do
+    if type(v) == "boolean" then
+      if #k == 1 then
+        table.insert(oneflags, k)
+      else
+        table.insert(longflags, string.format("--%s", k))
+      end
+    else
+      table.insert(equalflags, string.format("--%s='%s'", k, v))
+    end
+  end
+
+  -- write short flags
+  h:write(table.format("-%s ", table.concat(oneflags)))
+
+  -- write long flags
+  for i = 1, #longflags do
+    local f = longflags[i]
+    h:write(f .. ' ')
+  end
+
+  -- write equal flags
+  for i = 1, #equalflags do
+    local f = equalflags[i]
+    h:write(f .. ' ')
+  end
+
+  -- write the number of steps completed.
+  h:write('\n' .. tostring(steps) .. '\n')
+
+  -- write the position and facing
+  h:write(string.format("%d %d %d %d\n", pos.x, pos.y, pos.z, pos.facing))
+
+  -- write the starting position
+  h:write(string.format("%d %d %d %d", start.x, start.y, start.z, start.facing))
+
+  h:close()
 end
 
 --- Dig a room.
